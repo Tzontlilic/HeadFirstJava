b09b38280f4ecd14c52946371f5e0bbdeed564af
Tzontlilic <767406972@qq.com>
2019-07-23 21:06:25 +0800

    2019/7/23 21:06

--- ch7/OverLoad/OverLoads.java
+++ ch7/OverLoad/OverLoads.java
@@ -4,6 +4,11 @@ public class OverLoads {
 	// 重载，重载的定义是 使用相同的方法名称，但可以有不同的返回类型，或者不同类型的形参
 
 	// 不同的返回类型	
+	// 
+	// 重载的合法规范
+	// 1 返回类型可以不同
+	// 2 不能只改变返回类型
+	// 3 可以更改存取权限
 	public int addNums(int a ,int b) {
 		
 		return a + b;
@@ -20,6 +25,9 @@ public class OverLoads {
 	}
 	//**********end***********//
 	
+
+
+
 	// 不同的形参
 	
 	public void setUniqueID(String theID) {
@@ -33,4 +41,5 @@ public class OverLoads {
 		setUniqueID(theIDString);
 
 	}
+	//***********end************//
 }
