487b58e987ee8e7d01c7409062b14adba0b6cb2b
Tzontlilic <767406972@qq.com>
2019-08-21 21:26:32 +0800

    2019/8/21

---
+++ GeekTest/test/Test.java
@@ -0,0 +1,16 @@
+public class Test {
+	
+	public static void main(String args[]) {
+	
+
+		student s = new student();
+	}
+}
+
+class student {
+	
+	student() {
+	
+		System.out.println("11");
+	}
+}
